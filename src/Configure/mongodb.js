0fe1c63ac53152326607ad4cd7cf0d9ab5127696
Simon Favour <95985027+Lomoee@users.noreply.github.com>
2023-03-16 09:26:14 +0100

    Update mongodb.js

--- src/Configure/mongodb.js
+++ src/Configure/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
 mongoose
-.connect('mongodb+srv://favour:favoursu@cluster0.1i4m3zl.mongodb.net/foodweb')
+.connect('****')
 .then(() => console.log(`connected to DB`))
 .catch((err) => console.log(err));
